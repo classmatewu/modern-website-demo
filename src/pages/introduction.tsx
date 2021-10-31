4c9af70e87792bb96a2236249c2f12c02ff28f5e
classmatewu <171755422@qq.com>
2021-10-31 21:41:50 +0800

    feat: 添加局部CSR(NoSSR)

--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -1,6 +1,8 @@
 import getData from '@api/data';
 import { useLoader } from '@modern-js/runtime';
+import { NoSSR } from '@modern-js/runtime/ssr';
 import styled from '@modern-js/runtime/styled';
+import { useEffect, useState } from 'react';
 
 const fontSize = '30px';
 
@@ -17,7 +19,24 @@ const Introduction = () => {
   });
   // console.log('---data---', data);
 
-  return <Title>Introduction page {data?.title}</Title>;
+  const [time, setTime] = useState(new Date().toString());
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date().toString());
+    }, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Title>Introduction page {data?.title}</Title>
+      <NoSSR>
+        <div>Current time: {time}</div>
+      </NoSSR>
+    </div>
+  );
 };
 
 export default Introduction;
