e82b7a41d16923df8f1e30a8e72533c1b4a98bed
classmatewu <171755422@qq.com>
2021-10-31 21:24:00 +0800

    feat: 开启ssr，返回的html带上数据了

---
+++ api/data.ts
@@ -0,0 +1,5 @@
+export default async () => ({
+  title: 'Introduction to Modern.js',
+  content:
+    'Modern.js is a modern web development solution. It offers best practice for different scenarios，such as mobile pages, websites, API services, etc.',
+});
--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -1,3 +1,5 @@
+import getData from '@api/data';
+import { useLoader } from '@modern-js/runtime';
 import styled from '@modern-js/runtime/styled';
 
 const fontSize = '30px';
@@ -8,6 +10,14 @@ const Title = styled.div`
   text-align: center;
 `;
 
-const Introduction = () => <Title>Introduction page</Title>;
+const Introduction = () => {
+  const { data } = useLoader(async () => {
+    const res = await getData();
+    return res;
+  });
+  // console.log('---data---', data);
+
+  return <Title>Introduction page {data?.title}</Title>;
+};
 
 export default Introduction;
