f60598c8d05f0db9018fac26f77bf8f956f453aa
classmatewu <171755422@qq.com>
2021-10-31 16:53:45 +0800

    feat: add css in js

--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -1,3 +1,13 @@
-const Introduction = () => <div>Introduction page</div>;
+import styled from '@modern-js/runtime/styled';
+
+const fontSize = '30px';
+
+const Title = styled.div`
+  font-size: ${fontSize};
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Introduction = () => <Title>Introduction page</Title>;
 
 export default Introduction;
